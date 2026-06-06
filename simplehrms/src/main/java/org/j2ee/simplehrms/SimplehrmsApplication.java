658c5acf2e3a7520dae1d70754b6fbc59f9ca1b2
AmirReza <amirezasalehi75@gmail.com>
2026-06-06 10:18:53 +0330

    changed to spring boot

---
+++ simplehrms/src/main/java/org/j2ee/simplehrms/SimplehrmsApplication.java
@@ -0,0 +1,13 @@
+package org.j2ee.simplehrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplehrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplehrmsApplication.class, args);
+	}
+
+}
---
+++ simplehrms/src/test/java/org/j2ee/simplehrms/SimplehrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.j2ee.simplehrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplehrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
