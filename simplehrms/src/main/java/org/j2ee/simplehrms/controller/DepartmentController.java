d2019a568d4ea90f88d6d572432990fd445cae4d
AmirReza <amirezasalehi75@gmail.com>
2026-06-07 13:54:26 +0330

    add DepartmentController

--- simplehrms/src/main/java/org/j2ee/simplehrms/controller/DepartmentController.java
+++ simplehrms/src/main/java/org/j2ee/simplehrms/controller/DepartmentController.java
@@ -1,10 +1,15 @@
 package org.j2ee.simplehrms.controller;
 
+import org.j2ee.simplehrms.dto.DepartmentDTO;
 import org.j2ee.simplehrms.service.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 // base URL: /api/departments
@@ -21,6 +26,14 @@ public class DepartmentController {
     // CREATE 
     // POST /api/departments
 
-    public ResponseEntity<
+    public ResponseEntity<DepartmentDTO.Response> create(
+        @Valid @RequestBody DepartmentDTO.CreateRequest request
+    ){
+        DepartmentDTO.Response created = departmentService.create(request);
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(created);
+    }
 
 }
